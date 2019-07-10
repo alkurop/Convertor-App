6fc304b286c77f08f4d089c804001b3707f8b7ca
Alex Kuropiatnyk <alkurop@gmail.com>
2019-07-10 21:25:01 +0200

    add donate button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 
 import FlacToWavConvertorComponent from "./file_convertor/FlacToWavConvertorComponent";
+import DonateComponent from "./DonateComponent";
 
 function App() {
   return (
@@ -10,10 +11,14 @@ function App() {
         <img className="Logo" src="/flac.png" alt="Flac logo" />
         <h1>Convert Flac to Wav</h1>
         <FlacToWavConvertorComponent />
-        {/* <FinishedComponent item={{fileName:"test", url:"test"}} /> */}
-        <a className="email" href="mailto:alkurop@gmail.com">
-          alkurop@gmail.com
-        </a>
+        <div className="other">
+          <a className="stuff" href="mailto:alkurop@gmail.com">
+            alkurop@gmail.com
+          </a>
+          <div className="stuff" >
+            <DonateComponent />
+          </div>
+        </div>
       </div>
     </div>
   );
